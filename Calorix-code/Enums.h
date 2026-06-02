435a65bc1284e57ed9a9f2239fec4952955eea6b
SophiaRaytchinova <sraychinova@gmail.com>
2026-06-03 00:16:13 +0300

    added needed enums

---
+++ Calorix-code/Enums.h
@@ -0,0 +1,25 @@
+#pragma once
+
+enum activityLevel {
+    SEDENTARY,
+    LIGHT,
+    MODERATE,
+    ACTIVE,
+    VERY_ACTIVE
+}; 
+
+enum goalType {
+    WEIGHT_LOSS,
+    BULKING,
+    MAINTENANCE
+};
+
+enum muscleGroup {
+    CHEST,
+    BACK,
+    LEGS,
+    SHOULDERS,
+    ARMS,
+    CORE,
+    CARDIO
+};
