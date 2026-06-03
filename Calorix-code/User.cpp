2e458b4bb6f785e9f45d828f56b7dcd2b44ec304
SophiaRaytchinova <sraychinova@gmail.com>
2026-06-03 12:47:54 +0300

    added help function

--- Calorix-code/User.cpp
+++ Calorix-code/User.cpp
@@ -1,4 +1,6 @@
 #include "User.h"
+#include <iostream>
+
 int User::nextId = 0;
 
 User::User(const std::string& username_, const std::string& password_, const UserProfile& profile_) : username(username_), password(password_), profile(profile_)
@@ -21,6 +23,11 @@ double User::getAge() const
     return profile.getAge();
 }
 
+std::string User::getGender() const
+{
+    return profile.getGender();
+}
+
 void User::registerUser(std::string username, std::string password, size_t age, size_t weigth, size_t height, std::string gender)
 {
 
@@ -33,10 +40,13 @@ void User::login(std::string username, std::string password)
 
 void User::logout()
 {
-
+    
 }
 
 void User::help()
 {
-
+    std::cout << "Available commands:" << std::endl;
+    std::cout << "1. register - Register a new user" << std::endl;
+    std::cout << "2. login - Log in to your account" << std::endl;
+    std::cout << "3. logout - Log out of your account" << std::endl;
 }
