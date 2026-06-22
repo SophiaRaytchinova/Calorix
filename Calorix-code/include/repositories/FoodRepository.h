18c61647ed0ed4b4fe43767671dea524d4c0cb28
SophiaRaytchinova <sraychinova@gmail.com>
2026-06-22 20:47:41 +0300

    Add comment for myself

--- Calorix-code/include/repositories/FoodRepository.h
+++ Calorix-code/include/repositories/FoodRepository.h
@@ -11,7 +11,7 @@ public:
     void clear();
     void addFood(const Food& food);
     bool exists(const std::string& name) const;
-    std::shared_ptr<Food> findFood(const std::string& name) const;
+    std::shared_ptr<Food> findFood(const std::string& name) const; //връща shared защото и foodEntry трябва са сочи към същия пойнтър
     void updateFoodCalories(const std::string& name, double newCalories);
     const std::vector<std::shared_ptr<Food>>& getFoods() const;
 };
