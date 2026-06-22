965e13467f923091b72cd6ccdaefb7cab51b9087
SophiaRaytchinova <sraychinova@gmail.com>
2026-06-22 22:45:22 +0300

    Fix include path

--- Calorix-code/src/utils/Serializer.cpp
+++ Calorix-code/src/utils/Serializer.cpp
@@ -1,4 +1,4 @@
-#include "../../include/core/Serializer.h"
+#include "../../include/utils/Serializer.h"
 #include "../../include/core/Calorix.h"
 
 void Serializer::save(const Calorix& system, const std::string&) 
