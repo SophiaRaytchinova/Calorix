fde098e37009692f54fd203ebe7a97ba2bf9f34e
SophiaRaytchinova <sraychinova@gmail.com>
2026-06-22 20:41:04 +0300

    Fix typo

--- Calorix-code/include/utils/WorkoutGenerator.h
+++ Calorix-code/include/utils/WorkoutGenerator.h
@@ -5,6 +5,9 @@
 
 class WorkoutGenerator {
 public:
-    static std::vector<std::shared_ptr<Exercise>> generatePlan(const std::vector<std::shared_ptr<Exercise>>& exercises, int durationMinutes);
+    static std::vector<std::shared_ptr<Exercise>> generatePlan(
+        const std::vector<std::shared_ptr<Exercise>>& exercises, 
+        int durationMinutes
+    );
     static double planCalories(const std::vector<std::shared_ptr<Exercise>>& plan);
 };
